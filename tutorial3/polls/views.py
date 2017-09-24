dce6574db28afd5c0428844f069e0c2d0c3e7304
Naveen krishna Murakonda <mnaveenkrishna1566@gmail.com>
2017-09-24 15:46:44 -0400

    now in part3 of polls app tutorial

--- tutorial3/polls/views.py
+++ tutorial3/polls/views.py
@@ -1,14 +1,23 @@
 from __future__ import unicode_literals
-from django.http import HttpResponseRedirect,HttpResponse
-from .models import Question,Choice
+from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render,get_object_or_404
+
+# Import the render function to directly load,request,template_name, context manager
+# Importing the Question class from the models
+
 from django.template import loader
-from django.urls import reverse
+# The django template loader to load the template into the program
 
 def index(request):
-    return HttpResponse("This is the index page")
+   question_list= Question.objects.all()
+   context = {"question_list":question_list}
+   return render(request,'polls/index.html',context)
 
 def detail(request,question_id):
-    return HttpResponse("These are the details to the question page with question_id" % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    context = {"question":question}
+    return render(request,'polls/detail.html',context)
 
 def results(request,question_id):
     return HttpResponse("This is the page that lists the results of the web page %s" % question_id)
