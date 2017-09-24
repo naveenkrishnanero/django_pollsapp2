39f586f193d37d2aba51011a64745cfe53f69a59
Naveen krishna Murakonda <mnaveenkrishna1566@gmail.com>
2017-09-24 17:26:25 -0400

    Final commit before changing it to generic views and branch it

--- tutorial3/polls/views.py
+++ tutorial3/polls/views.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Question,Choice
 from django.shortcuts import render,get_object_or_404
+from django.urls import reverse
 
 # Import the render function to directly load,request,template_name, context manager
 # Importing the Question class from the models
@@ -20,8 +21,23 @@ def detail(request,question_id):
     return render(request,'polls/detail.html',context)
 
 def results(request,question_id):
-    return HttpResponse("This is the page that lists the results of the web page %s" % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{"question":question})
+
 
 def vote(request,question_id):
-    return HttpResponse("This is the page where people vote %s" % question_id )
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice= question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        # return it to display form
+        context= {"question":question,
+                  "error_message":"You didn't select a choice",}
+        return render(request,'polls/detail.html',context)
+    else:
+        selected_choice.vote +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+
 
